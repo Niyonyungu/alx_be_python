f4379d9cdc5759f29e4eb65867e34136c405b9b6
vainqueur <vainqueurmg@gmail.com>
2025-05-25 01:06:36 +0200

    first commit

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 20
+
+sum = number1 + number2
+difference = number2 - number1
+product = number1 * number2 
+
+print("Addition of", number1 , "and" , number2 , "is" , sum)
+print("Substraction of", number2 , "and" , number1 , "is" , difference)
+print("Multiplication of", number1 , "and" , number2 , "is" , product)
