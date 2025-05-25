d94ef63fba656a8b9c8c705af9e05113e0f355c4
vainqueur <vainqueurmg@gmail.com>
2025-05-25 18:57:12 +0200

    Fix formatting of output messages in finance calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -6,5 +6,5 @@ interest_rate = 0.05
 
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * interest_rate)
 
-print("Your monthly savings are $" , monthly_savings)
-print("Projected savings after one year, with interest, is:", "$", projected_savings)
+print("Your monthly savings are $" , monthly_savings , ".")
+print("Projected savings after one year, with interest, is: $", projected_savings , ".")
